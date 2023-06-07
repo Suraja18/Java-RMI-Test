691276048416d09f825829a45aaa62efd2d44f77
Suraj Adhikari <sssadhikari222@gmail.com>
2023-06-07 11:38:45 +0545

    Complete Prime Number Check

--- src/Client_window.java
+++ src/Client_window.java
@@ -3,20 +3,21 @@ import java.net.*;
 import java.util.*;
 
 public class Client_window {
-    public static void main (String args[]){
+    public static void main (String[] args){
         String host="localhost";
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter any Number");
         int a = scanner.nextInt();
         try{
             Prime_Num obj = (Prime_Num)Naming.lookup("rmi://"+host+"/Prime_Num");
-            System.out.println(obj.Prime_Num_Check(a));
-        } catch (MalformedURLException e) {
+            if (obj.Prime_Num_Check(a) == 1){
+                System.out.println("The number: "+a +" is Prime Number");
+            }
+            else if (obj.Prime_Num_Check(a) == 0){
+                System.out.println("The number: "+a +" is not Prime Number");
+            }
+        } catch (MalformedURLException | NotBoundException | RemoteException e) {
             e.printStackTrace();
-        } catch (NotBoundException ef) {
-            ef.printStackTrace();
-        } catch (RemoteException efg) {
-            efg.printStackTrace();
         }
     }
 }
